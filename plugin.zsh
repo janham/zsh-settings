7900992be0b45f1492f77c55ba8f614a6963e13e
janham <janhampino@gmail.com>
2024-07-02 12:30:11 +0900

    bat plugin installed

--- plugin.zsh
+++ plugin.zsh
@@ -1,1 +1,13 @@
 zinit ice depth=1; zinit light romkatv/powerlevel10k
+zinit light paulirish/git-open
+zinit light zdharma/fast-syntax-highlighting
+
+### bat plugin
+zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
+zinit light sharkdp/bat
+
+# 以下はただのエイリアス設定
+if builtin command -v bat > /dev/null; then
+  alias cat="bat"
+fi
+###
